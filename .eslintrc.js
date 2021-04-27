fe13d62e83cfc98d46632829f9fe643134b90c20
フズキ <hades785@gmail.com>
2021-04-27 02:14:05 +0200

    Setup vue-i18n

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,44 @@
 module.exports = {
   root: true,
+
   env: {
     node: true,
   },
+
   extends: [
-    "plugin:vue/vue3-essential",
     "eslint:recommended",
+    "plugin:vue/vue3-essential",
+    "plugin:@intlify/vue-i18n/recommended",
     "@vue/typescript/recommended",
     "@vue/prettier",
     "@vue/prettier/@typescript-eslint",
   ],
+
   parserOptions: {
     ecmaVersion: 2020,
   },
+
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-console": "off",
+    "no-debugger": "off",
+
+    // vue-i18n
+    "@intlify/vue-i18n/no-dynamic-keys": "error",
+    "@intlify/vue-i18n/no-unused-keys": [
+      "error",
+      {
+        extensions: [".js", ".vue"],
+      },
+    ],
+  },
+
+  settings: {
+    "vue-i18n": {
+      localeDir: "src/i18n/*.{json,json5,yaml,yml}",
+
+      // Specify the version of `vue-i18n` you are using.
+      // If not specified, the message will be parsed twice.
+      messageSyntaxVersion: "^9.0.0",
+    },
   },
 };
---
+++ src/components/HelloI18n.vue
@@ -0,0 +1,13 @@
+<template>
+  <p>{{ $t("hello") }}</p>
+  <p>Boop?</p>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({
+  name: "HelloI18n",
+})
+export default class HelloI18n extends Vue {}
+</script>
