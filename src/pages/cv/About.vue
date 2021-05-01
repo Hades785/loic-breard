e25d8c8742bc13f0c69ff7585ae0b0eeca71af92
フズキ <hades785@gmail.com>
2021-05-02 01:39:05 +0200

    Ported 'About' content from existing website

--- src/pages/cv/About.vue
+++ src/pages/cv/About.vue
@@ -1,3 +1,40 @@
 <template>
   <h2>{{ $t("about.title") }}</h2>
+  <p>{{ $t("about.paragraph") }}</p>
+  <div>
+    {{ $t("about.email") }}
+    <a :href="`mailto:${email}`">{{ email }}</a>
+  </div>
+  <div>
+    {{ $t("about.portfolio") }}
+    <a :href="itch">{{ itch }}</a>
+  </div>
+  <div>
+    {{ $t("about.cv") }}
+    <a :href="cv">{{ cv }}</a>
+  </div>
+  <div>
+    {{ $t("about.linkedin") }}
+    <a :href="linkedin">{{ linkedin }}</a>
+  </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  data() {
+    const email = "loic.breard@outlook.com";
+    const itch = "https://hades785.itch.io/";
+    const cv = "https://loic-breard.ovh/cv.pdf";
+    const linkedin = "https://linkedin.com/in/loïc-bréard-69978b157/";
+
+    return {
+      email,
+      itch,
+      cv,
+      linkedin,
+    };
+  },
+});
+</script>
