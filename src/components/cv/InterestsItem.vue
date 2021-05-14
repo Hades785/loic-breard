c1c8ef9053b3f1682348a01f38afec9e462f213d
フズキ <hades785@gmail.com>
2021-05-14 12:57:05 +0200

    Added cv/interests contents

---
+++ src/components/cv/InterestsItem.vue
@@ -0,0 +1,18 @@
+<template>
+  <generic-item :titleLine="title" :firstLine="description"></generic-item>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import GenericItem from "./GenericItem.vue";
+
+export default defineComponent({
+  props: {
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+  },
+  components: {
+    GenericItem,
+  },
+});
+</script>
--- src/pages/cv/Interests.vue
+++ src/pages/cv/Interests.vue
@@ -1,3 +1,32 @@
 <template>
   <h2>{{ $t("interests.title") }}</h2>
+  <ul>
+    <interests-item
+      :title="$t('interets.japan.title')"
+      :description="$t('interests.japan.description')"
+    ></interests-item>
+    <interests-item
+      :title="$t('interets.games.title')"
+      :description="$t('interests.games.description')"
+    ></interests-item>
+    <interests-item
+      :title="$t('interets.music.title')"
+      :description="$t('interests.music.description')"
+    ></interests-item>
+    <interests-item
+      :title="$t('interets.sys-admin.title')"
+      :description="$t('interests.sys-admin.description')"
+    ></interests-item>
+  </ul>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import InterestsItem from "@/components/cv/InterestsItem.vue";
+
+export default defineComponent({
+  components: {
+    InterestsItem,
+  },
+});
+</script>
